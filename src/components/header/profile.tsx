d54966e8962227bc46c6eb16d55b9d1f5f491dc7
Margi Godhasara <margi@pansophictech.com>
2024-09-03 15:23:23 +0530

    Designs header components

--- src/components/header/profile.tsx
+++ src/components/header/profile.tsx
@@ -3,7 +3,7 @@ import { Avatar, Flex } from "@pansophictech/base";
 
 const Profile: FC<{ alt?: any; src?: any }> = ({ alt, src }) => {
   return (
-    <Flex align={'center'} justify={"center"} h="80px">
+    <Flex align={"center"} justify={"center"} h="80px">
       <Avatar src={src} alt={alt} size="md" />
     </Flex>
   );
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,31 +1,31 @@
 export const TABSDATA = [
   {
-    value: "enquiry",
-    label: "Enquiry",
+    value: "guests",
+    label: "Guests",
     // icon: <IconPhoto size={16} />,
     content: "This is the content for the Gallery tab.",
   },
   {
-    value: "leads",
-    label: "Leads",
+    value: "assessment",
+    label: "Assessment",
     // icon: <IconMessageCircle size={16} />,
     content: "This is the content for the Messages tab.",
   },
   {
-    value: "opportunity",
-    label: "Opportunity",
+    value: "gnome",
+    label: "GNOME",
     // icon: <IconSettings size={16} />,
     content: "This is the content for the Settings tab.",
   },
   {
-    value: "clients",
-    label: "Clients",
+    value: "schedule",
+    label: "Schedule",
     // icon: <IconSettings size={16} />,
     content: "This is the content for the Set Data tab.",
   },
   {
-    value: "clientsData",
-    label: "Clients Data",
+    value: "payments",
+    label: "Payments",
     // icon: <IconSettings size={16} />,
     content:
       "This is the content for the Set Data tab Clients Data Clients Data.",
