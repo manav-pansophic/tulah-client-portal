4c9362dcef6194e98cc90ef3f8766f40da45bb39
Margi Godhasara <margi@pansophictech.com>
2024-09-03 21:05:04 +0530

    Add design changes

---
+++ src/components/Gnome/Instruction.tsx
@@ -0,0 +1,26 @@
+import { Paper, Text } from "@pansophictech/base";
+import { FC } from "react";
+
+const Instruction: FC<{
+  instructionTitle?: string;
+  instructionList?: { list: string }[];
+}> = ({ instructionTitle, instructionList }) => {
+  return (
+    <Paper
+      className="layout-bg-color"
+      style={{
+        backdropFilter: "blur(40px)",
+      }}
+      radius={"sm"}
+    >
+      <Text fw={600}>{instructionTitle}</Text>
+      {instructionList?.map((item, index) => (
+        <Text key={index} size="xs" pt="xs">
+          {item.list}
+        </Text>
+      ))}
+    </Paper>
+  );
+};
+
+export default Instruction;
