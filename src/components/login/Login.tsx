d84224b43e6055a12d9d2585dfd344cdfb98e021
Margi Godhasara <margi@pansophictech.com>
2024-09-09 14:55:18 +0530

    Add login screen design

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import LoginForm from '../forms/login/LoginForm';
+import LoginForm from "../forms/login/LoginForm";
 
 const Login = () => {
   return (
-    <div style={{backdropFilter: 'blur(10px)'}}>
+    <div style={{ backdropFilter: "blur(10px)" }}>
       <LoginForm />
     </div>
   );
